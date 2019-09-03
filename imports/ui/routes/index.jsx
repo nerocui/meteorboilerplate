257474d2a921d2fc323120d9ccae845112bad2ac
Nero Cui <nerocui@outlook.com>
2019-09-03 13:32:27 -0700

    removed chatthread page

--- imports/ui/routes/index.jsx
+++ imports/ui/routes/index.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-d
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import AuthPage from "../pages/authPage";
 import ChatListPage from '../pages/chatListPage';
-import ChatThreadPage from '../pages/chatThreadPage';
 import MomentsPage from '../pages/momentsPage';
 import ContactsListPage from '../pages/contactsPage';
 import MePage from '../pages/mePage';
@@ -38,7 +37,6 @@ const routes = (
 										render={() => (
 											<Switch>
 												<Route exact path='/chatlist' component={ChatListPage} />
-												<Route exact path='/chatthread' component={ChatThreadPage} />
 												<Route exact path='/contacts' component={ContactsListPage} />
 												<Route exact path='/moments' component={MomentsPage} />
 												<Route exact path='/me' component={MePage} />
